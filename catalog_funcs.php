890c7dd1696a63d3c4220f8e223740106f6348c3
Max <maxya@mail.ru>
2020-04-12 10:52:31 +0300

    Update catalog_funcs.php

--- catalog_funcs.php
+++ catalog_funcs.php
@@ -4,9 +4,9 @@ $vendors_curr_i_down = get_vars_catalogue ( $folder, "vendors_currency" );
 if (! $vendors_curr_i_down) {
 	$vendors_curr_i_down = array ();
 	
-        $result = $db->query ( "SELECT vc.currency_id, vc.rate, name,vendor_id FROM `" . $tabl_prefix . "_vendors_currency` vc LEFT JOIN `" . $tabl_prefix . "_currency` c ON (c.id = vc.currency_id) WHERE `vendor_id` IN (SELECT `id` FROM `" . $tabl_prefix . "_vendors` ORDER BY `id` ASC) ORDER BY `currency_id` ASC" );
+        $result = $db->query ( "SELECT vc.currency_id, vc.rate, name,vendor_id FROM `" . $tabl_prefix . "_vendors_currency` vc LEFT JOIN `" . $tabl_prefix . "_currency` c ON (c.id = vc.currency_id) WHERE `vendor_id` IN (SELECT `id` FROM `" . $tabl_prefix . "_vendors`) ORDER BY `currency_id` ASC" );
 	    while ( $row = $db->get_row ( $result ) ) {
-	        $id_v = $row['id'];
+	        $id_v = $row['vendor_id'];
             $vendors_curr_i_down [$id_v]= array ();
             $vendors_curr_i_down [$id_v][$row['currency_id']] = array();
 		    foreach ( $row as $key => $value ) {
